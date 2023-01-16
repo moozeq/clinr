3bec21d445b0d0556fcb00907b990513b15c0200
Amon <j.niedzw@gmail.com>
2023-01-16 22:28:50 +0100

    [#16] Changed `sessionStorage` to `localStorage` to persist logged user between tabs.

--- frontend/src/app/token-storage.service.ts
+++ frontend/src/app/token-storage.service.ts
@@ -7,27 +7,29 @@ import { LoggedUser, LoggedUserDto } from './dto/user.dto';
 export class TokenStorageService {
   private authTokenKey = 'auth-token';
   private authUserKey = 'auth-user';
+  private storage = window.localStorage;
+
   constructor() { }
 
   signOut(): void {
-    window.sessionStorage.removeItem(this.authTokenKey);
-    window.sessionStorage.removeItem(this.authUserKey);
+    this.storage.removeItem(this.authTokenKey);
+    this.storage.removeItem(this.authUserKey);
   }
 
   saveToken(token: string): void {
-    window.sessionStorage.setItem(this.authTokenKey, token);
+    this.storage.setItem(this.authTokenKey, token);
   }
 
   getToken(): string | null {
-    return window.sessionStorage.getItem(this.authTokenKey);
+    return this.storage.getItem(this.authTokenKey);
   }
 
   saveUser(user: LoggedUser): void {
-    window.sessionStorage.setItem(this.authUserKey, JSON.stringify(user));
+    this.storage.setItem(this.authUserKey, JSON.stringify(user));
   }
 
   getUser(): LoggedUser | null {
-    const user = window.sessionStorage.getItem(this.authUserKey);
+    const user = this.storage.getItem(this.authUserKey);
     if (user) {
       return new LoggedUser(JSON.parse(user) as LoggedUserDto);
     }
